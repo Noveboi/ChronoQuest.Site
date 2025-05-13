df46c7cbda6125360c94abd1ce25bd5549264f5a
George Nikolaidis <nove@novesoftware.net>
2025-05-13 21:59:33 +0300

    Add .types files

---
+++ src/lib/types/domain.types.ts
@@ -0,0 +1,13 @@
+/*
+    DTOs used to communicate with the back-end.
+*/
+
+export interface User {
+    username: string
+}
+
+export interface Chapter {
+    id: string,
+    title: string,
+    content: string
+} 
