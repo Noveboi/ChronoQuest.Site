d81aa4e1496a0a7dcf74cf9c7b145f82a6ddcf34
George Nikolaidis <nove@novesoftware.net>
2025-05-13 22:00:35 +0300

    Add demo playground to test API login and endpoints

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,68 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { apiGet } from "$lib/services/apiService";
+    import { auth, login } from "$lib/stores/auth.svelte";
+    import type { Chapter } from "$lib/types/domain.types";
+    import type { LoginRequest } from "$lib/types/requests.types";
+
+    const request: LoginRequest = $state({
+        email: '',
+        password: ''
+    })
+
+    let chapters: readonly Chapter[] = $state([])
+
+    function onsubmit(e: SubmitEvent) {
+        e.preventDefault()
+        login(request)
+    }
+
+    async function getChapters() {
+        const response = await apiGet<Chapter[]>('/chapters')
+        chapters = response;
+    }
+</script>
+
+<form onsubmit={onsubmit}>
+    <label for="email">Email</label>
+    <input id="email" type="email" bind:value={request.email}>
+
+    <label for="pass">Password</label>
+    <input id="pass" type="password" bind:value={request.password}>
+
+    <button type="submit">Sign In</button>
+</form>
+
+<p>Authenticated: {auth.isAuthenticated}</p>
+<p>Loading: {auth.isLoading}</p>
+<p>Token: {auth.token}</p>
+
+<div class="container">
+    <button type="button" onclick={getChapters}>Get Example Chapters from Back-end</button>
+    <ul>
+        {#if chapters.length === 0}
+            <h1>No chapters found!</h1>
+        {/if}
+        {#each chapters as chapter}
+            <li>
+                {chapter.title} - "{chapter.content}"
+            </li>
+        {/each}
+    </ul>
+</div>
+
+<style lang="scss">
+    .container {
+        padding: 1rem;
+        background-color: purple;
+        color: white;
+
+        > button {
+            all: unset;
+            background-color: orange;
+            color: darkmagenta;
+            user-select: none;
+            padding: 0.5rem 1.25rem;
+            cursor: pointer;
+        }
+    }
+</style>
