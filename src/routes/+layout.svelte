9ef94b725dc605742c1458656abc6d017709706d
MariaDim03 <marydimi03@gmail.com>
2025-05-28 22:35:54 +0300

    -Background Test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,17 @@
     let { children } = $props()
 </script>
 
-<h2 class={auth.isAuthenticated ? 'auth' : 'authnt'}>
-    {#if auth.isAuthenticated}
-        Authenticated 😁🥳
-    {:else}
-        Not Authenticated 😭
-    {/if}
-</h2>
-<div class="demo-layout-container">
-    {@render children()}
+<div class="gradient-background">
+    <h2 class={auth.isAuthenticated ? 'auth' : 'authnt'}>
+        {#if auth.isAuthenticated}
+            Authenticated 😁🥳
+        {:else}
+            Not Authenticated 😭
+        {/if}
+    </h2>
+    <div class="demo-layout-container">
+        {@render children()}
+    </div>
 </div>
 
 <style>
@@ -23,4 +25,28 @@
 
     .auth { color: green; font-weight: bolder; }
     .authnt { color: red;}
+
+    html, body {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+    }
+
+    :root {
+    --blue: #2875F9;
+    --green: #28D223;
+  }
+  
+    .gradient-background{
+        background: radial-gradient(ellipse at bottom right, var(--blue), var(--green));
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        height: 100vh;
+        width: 100vw;
+        margin: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 </style>
