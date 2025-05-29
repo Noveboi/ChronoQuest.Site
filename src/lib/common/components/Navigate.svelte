880d7a852116a60e8c12928a34ed610af58307d5
Nove <cptgiorgos03@gmail.com>
2025-05-30 02:54:21 +0300

    Add `Navigate` button component for routing

---
+++ src/lib/common/components/Navigate.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Button from "./Button.svelte";
+    import type { Snippet } from "svelte";
+
+    type NavigateProps = { to: string, children: Snippet }
+
+    const { to, children }: NavigateProps = $props();
+</script>
+
+<a href={to}>
+    <Button children={children}>
+    </Button>
+</a>
