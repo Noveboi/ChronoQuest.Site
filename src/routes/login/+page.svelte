a324aa21f174a291df782a72c6a10a672adf4fc6
MariaDim03 <marydimi03@gmail.com>
2025-05-29 18:00:04 +0300

    -Form Component & Form Page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+import Title from "$lib/features/chapters/components/Title.svelte";
+import Form from "$lib/features/chapters/components/Form.svelte";
+import Button from "$lib/features/chapters/components/Button.svelte";
+
+function handleClick() {
+alert("Button clicked!");
+}
+</script>
+
+<Title text="Login" size="4em" />
+
+<div class="login-container">
+    <Form />
+    <Button on:click={handleClick} >
+    Enter
+    </Button>
+</div>
+
+<style>
+.login-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>
