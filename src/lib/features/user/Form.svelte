4d8ac48b6ecd4aa0fe4c668a3df0c871c15e2818
Nove <cptgiorgos03@gmail.com>
2025-05-30 02:56:14 +0300

    Move & make <Form> functional

---
+++ src/lib/features/user/Form.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import Button from "$lib/common/components/Button.svelte";
+    import { auth } from "$lib/common/stores/auth.svelte";
+
+  type FormProps = { onSubmit: (email: string, password: string) => Promise<void> }
+
+  const { onSubmit }: FormProps = $props();
+  let email = $state('');
+  let password = $state('');
+
+  async function handleSubmit() {
+    const isSuccess = await onSubmit(email, password);
+  }
+
+</script>
+
+<form class="form-container" onsubmit={handleSubmit}>
+  <input type="text" placeholder="Email" bind:value={email} />
+  <input type="password" placeholder="Password" bind:value={password} />
+  <Button type="submit">Enter</Button>
+</form>
+
+{#if auth.error}
+  <p class="error">{auth.error}</p>
+{/if}
+
+<style>
+  .form-container {
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin: 0 auto;
+  }
+
+  .form-container input {
+    padding: 0.75em;
+    border: 1px solid #ccc;
+    border-radius: 0.5rem;
+    font-size: 1rem;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+  }
+</style>
