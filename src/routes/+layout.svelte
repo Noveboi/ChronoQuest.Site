cb7cbf6fa01d2f8104f94a751045f1d325f5ed07
Nove <cptgiorgos03@gmail.com>
2025-05-30 03:55:00 +0300

    Add logout button and redesign layout structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,29 @@
 <script lang="ts">
     import { isAuthenticated } from '$lib/common/guards/routeGuard.svelte';
+    import { auth, logout } from '$lib/features/auth/auth.svelte';
     import type { LayoutProps } from './$types';
+    import { SvelteToast } from '@zerodevx/svelte-toast';
 
     let { data, children }: LayoutProps = $props()
 
     $effect(() => isAuthenticated(data.url)())
 </script>
 
-<div class="gradient-background">
-    {@render children()}
+<div class="gradient-background"></div>
+
+<div class="app-root">
+    {#if auth.isAuthenticated}
+        <button type="button" style="position: absolute" onclick={logout}>Logout</button>
+    {/if}
+    
+    <main class="content">
+        {@render children()}
+    </main>
 </div>
+
+<SvelteToast options={{
+    
+}}/>
 
 <style>
     .gradient-background{
@@ -20,9 +34,24 @@
         height: 100dvh;
         width: 100dvw;
         margin: 0;
+        z-index: -1;
+        position: fixed;
+    }
+
+    .app-root {
+        padding: 0;
+        margin: 0;
+        height: 100dvh;
+        width: 100dvw;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .content {
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        flex: 1;
     }
 </style>
