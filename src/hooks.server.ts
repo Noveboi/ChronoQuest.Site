0a28c5748d47e0a1777b503f43c9af1ae8c6264b
Nove <cptgiorgos03@gmail.com>
2025-06-02 21:34:33 +0300

    Add auth guards in server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,22 @@
+import { api } from "$lib/common/backend";
 import { cookieName } from "$lib/features/auth/auth.constants";
-import type { HandleFetch } from "@sveltejs/kit";
+import { redirect, type Handle, type HandleFetch } from "@sveltejs/kit";
+
+const unprotectedRoutes = ['login', 'register']
+
+export const handle: Handle = async ({event, resolve}) => {
+    const cookie = event.cookies.get(cookieName);
+    const notInUrl = (str: string) => !event.url.pathname.includes(str);
+
+    if (!cookie && unprotectedRoutes.every(notInUrl)) {
+        console.log('Unauthorized user, exiting and redirecting...')
+        await api(event.fetch).get('/exit');
+
+        redirect(307, '/login');
+    }
+    
+    return resolve(event);
+}
 
 export const handleFetch: HandleFetch = async ({event, fetch, request}) => {
     const { cookies } = event;
