18a3ca5d8315e680bed5744d32326aa0428ef395
Nove <cptgiorgos03@gmail.com>
2025-06-02 19:49:54 +0300

    Add disclaimer

--- src/routes/(app)/chapters/+layout.svelte
+++ src/routes/(app)/chapters/+layout.svelte
@@ -6,6 +6,11 @@
 </script>
 
 <div>
+    <!-- THESE LINKS ARE TEMPORARY! Just so we can navigate to all the chapters for debugging purposes. -->
+    <!-- THESE LINKS ARE TEMPORARY! Just so we can navigate to all the chapters for debugging purposes. -->
+    <!-- THESE LINKS ARE TEMPORARY! Just so we can navigate to all the chapters for debugging purposes. -->
+    <!-- THESE LINKS ARE TEMPORARY! Just so we can navigate to all the chapters for debugging purposes. -->
+    <!-- THESE LINKS ARE TEMPORARY! Just so we can navigate to all the chapters for debugging purposes. -->
     {#each data.chapters as chapter}
         <a data-sveltekit-preload-data='tap' href="/chapters/{chapter.id}">{chapter.title}</a> <br/>
     {/each}
