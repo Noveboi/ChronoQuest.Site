e2c6cae6585e140b9b8efdd8175df1d613a5204d
George Nikolaidis <nove@novesoftware.net>
2025-06-11 16:38:38 +0300

    Don't preload on Arrow hover

--- src/lib/common/components/Arrow.svelte
+++ src/lib/common/components/Arrow.svelte
@@ -11,7 +11,7 @@
     const { to, style, disabled = false, point = 'right' }: NavigateProps = $props();
 </script>
 
-<Navigate {to} {style} {disabled}>
+<Navigate {to} {style} {disabled} preload={false}>
     <svg class="point-{point}" xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 -960 960 960" fill="#e3e3e3">
         <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/>
     </svg>
