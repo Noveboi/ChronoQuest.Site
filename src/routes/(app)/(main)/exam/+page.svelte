bfffa261b4b04ac6b9435ed7101e3a838405d2d8
MariaDim03 <marydimi03@gmail.com>
2025-06-12 17:56:26 +0300

    -Exam Page

--- src/routes/(app)/(main)/exam/+page.svelte
+++ src/routes/(app)/(main)/exam/+page.svelte
@@ -1,11 +1,1 @@
-<script lang="ts">
-    import FinishedQuestionsModal from "$lib/features/questions/FinishedQuestionsModal.svelte";
-    import { getBackgroundState } from "$lib/features/ui/backgroundState.svelte";
-    import type { PageProps } from "./$types";
-
-    const {data}: PageProps = $props();
-    const backgroundState = getBackgroundState();
-</script>
-
-<FinishedQuestionsModal show={backgroundState} />
-
+<p>Exam Page</p>
