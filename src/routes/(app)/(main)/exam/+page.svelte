186cc30b241f29dad31b12b7b4c2ea7ec81661b6
MariaDim03 <marydimi03@gmail.com>
2025-06-13 21:38:20 +0300

    -Exam Page

--- src/routes/(app)/(main)/exam/+page.svelte
+++ src/routes/(app)/(main)/exam/+page.svelte
@@ -1,1 +1,11 @@
-<p>Exam Page</p>
+<script lang="ts">
+    import QuestLayout from "$lib/features/questions/layout/QuestLayout.svelte";
+    import type { PageProps } from "./$types";
+
+    const { data }: PageProps = $props();
+
+</script>
+
+<QuestLayout>
+    
+</QuestLayout>
